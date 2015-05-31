aa8cf4d7f1fae66b84e67776c4879638b8dfb9e5
syntaxfish <syntaxfish@gmail.com>
2015-06-01 02:21:17 +0900

    http service 추

--- httpServer.js
+++ httpServer.js
@@ -1,3 +1,17 @@
 /**
  * Created by syntaxfish on 15. 5. 28..
  */
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var cors = require('cors');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cors());
+app.use('/1/reviews', index);
+
+module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,49 @@
 var express = require('express');
 var router = express.Router();
 
-var review = require('../controllers/review');
+var getHeader = require('haru-nodejs-util').common.getHeader;
+var crawlerConfig = config.crawlerConfig;
+var RabbitMQ = require('../connectors/rabbitmq.js');
+var rabbitmq = new RabbitMQ();
+var async = require('async');
+var _ = require('underscore');
 
-router.post('/fetch', review.fetch);
+
+
+router.post('/fetch', function(req, res) {
+    var input = getHeader(req);
+
+    var options = req.body;
+    var markets = Object.keys(options);
+
+    async.times(markets.length, function(n, next) {
+        var market = markets[n];
+        var option = options[market];
+
+        option['market'] = market;
+
+        if( crawlerConfig[market] ){
+            option['locations'].forEach(function(location) {
+                if( _.contains(crawlerConfig[market], location) ) {
+                    var msg  = {
+                        market: market,
+                        page: 1,
+                        location: location,
+                        packageName: option.packageName,
+                        applicationId: input.applicationId
+                    };
+                    rabbitmq.publish('crawler', JSON.stringify(msg));
+                }
+            });
+        }
+
+        next(null, null);
+
+    },function done(error, results) {
+        res.json({});
+    });
+
+
+});
 
 module.exports = router;
